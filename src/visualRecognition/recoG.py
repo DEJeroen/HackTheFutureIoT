9a379700e3d758bc180292cbc6299f1f7916f09f
DEJeroen <jeroenrietveld482@gmail.com>
2016-12-08 14:00:34 +0100

    reverted

--- src/visualRecognition/recoG.py
+++ src/visualRecognition/recoG.py
@@ -21,6 +21,7 @@ def detectAlien():
 
 
     test_url = 'http://www.hdwallpapers.in/walls/alien-HD.jpg'
+    print(test_url)
     visual_recognition = VisualRecognitionV3('2016-05-20', api_key='243ed19843348fcdb81d320897e742f027887ab3')
     data=json.dumps(visual_recognition.classify(images_url=test_url,  threshold=0.6,
                                                          classifier_ids=['alien_630240897', 'default']), indent=2)
@@ -37,4 +38,4 @@ def detectAlien():
         exit(0)
 
 detectAlien()
-print("test")
+
