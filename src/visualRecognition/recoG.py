ec6a47fb8d89ecfc920f8c3427691ea9dbd566b5
DEJeroen <jeroenrietveld482@gmail.com>
2016-12-08 10:24:47 +0100

    Calibrated alien detection

--- src/visualRecognition/recoG.py
+++ src/visualRecognition/recoG.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 
-test_url = 'http://irobinhood.org/images/personImg.png'
+test_url = 'https://bitbucket-assetroot.s3.amazonaws.com/repository/9E6oEk/632489907-image2.jpg?Signature=XF1Lb%2BNZUyUr1GwMSuoNArBphCw%3D&Expires=1481189659&AWSAccessKeyId=AKIAIVFPT2YJYYZY3H4A&versionId=clIofJKnrYwxbyCuDVyXxHH20nk_nKCe'
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='243ed19843348fcdb81d320897e742f027887ab3')
-print(json.dumps(visual_recognition.classify(images_url=test_url,  threshold=0.1,
+print(json.dumps(visual_recognition.classify(images_url=test_url,  threshold=0.6,
                                                      classifier_ids=['alien_630240897', 'default']), indent=2))
