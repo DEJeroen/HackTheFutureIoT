37da09552aa98d4f93c066a8eaeb06e0827d0bb1
DEJeroen <jeroenrietveld482@gmail.com>
2016-12-08 11:28:56 +0100

    Print true if alien detected else false

--- src/visualRecognition/recoG.py
+++ src/visualRecognition/recoG.py
@@ -2,8 +2,18 @@ import json
 from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
+import requests
 
-test_url = 'https://bitbucket-assetroot.s3.amazonaws.com/repository/9E6oEk/632489907-image2.jpg?Signature=XF1Lb%2BNZUyUr1GwMSuoNArBphCw%3D&Expires=1481189659&AWSAccessKeyId=AKIAIVFPT2YJYYZY3H4A&versionId=clIofJKnrYwxbyCuDVyXxHH20nk_nKCe'
+test_url = 'http://www.hdwallpapers.in/walls/alien-HD.jpg'
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='243ed19843348fcdb81d320897e742f027887ab3')
-print(json.dumps(visual_recognition.classify(images_url=test_url,  threshold=0.6,
-                                                     classifier_ids=['alien_630240897', 'default']), indent=2))
+data=json.dumps(visual_recognition.classify(images_url=test_url,  threshold=0.6,
+                                                     classifier_ids=['alien_630240897', 'default']), indent=2)
+
+json1_data = json.loads(data)
+datapoints = json1_data['images'][0]
+classifiers=datapoints['classifiers']
+detected=classifiers.pop()['name']
+if detected == "alien":
+    print(True)
+else:
+    print(False)
